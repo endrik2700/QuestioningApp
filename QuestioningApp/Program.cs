4119bf08caaea7607d280987947b00ba38548418
Opilane TTHK <opilane@tthk.ee>
2019-10-18 18:24:26 +0300

    files

---
+++ QuestioningApp/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestioningApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string firstLetter;
+            string restOfName; 
+
+
+            Console.WriteLine("Hi! What's your name?");
+            name = Console.ReadLine();
+            firstLetter = name[0].ToString().ToUpper();
+            restOfName = name.Substring(1).ToLower();
+            name = firstLetter + restOfName;
+            Console.WriteLine($"Hello {name}! What's your gender- if male, then type:male, if woman, type: female");
+            string userAnswer = Console.ReadLine().ToLower();
+            UserTreater(userAnswer, name);
+        }
+        public static void UserTreater(string input, string userName)
+        {
+            int birthYear;
+            int age;
+            string comingOrLeaving;
+            
+
+
+            if (input == "male")
+            {
+                
+              
+                Console.WriteLine($" Hello Mr {userName}");
+
+            }
+            else if (input == "female")
+            {
+                Console.WriteLine($" Hello Mrs/Ms {userName}");
+            }
+            else {
+                Console.WriteLine("Something  is wrong, please check your input!");
+        }
+
+        Console.WriteLine("What's your year of birth?");
+            birthYear = int.Parse(Console.ReadLine());
+            age = 2019 - birthYear;
+        Console.WriteLine($"You are {age} years old. Are you coming or leaving-if coming, then write coming, if leaving, write leaving?");
+           comingOrLeaving = Console.ReadLine().ToLower();
+            if ( comingOrLeaving == "coming") {
+                Console.WriteLine("Welcome to the web page!");
+            }
+            else if (comingOrLeaving == "leaving") {
+                Console.WriteLine("Thanks for using the web page!Goodbye!");
+            }
+            else {
+                Console.WriteLine("Something is wrong, check your input!");
+            }
+            Console.ReadLine();
+           
+           
+    }
+
+    }
+}
+
---
+++ SpaceCounter/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userSentence;
+            string IDCode;
+            bool idValid;
+            
+
+            Console.WriteLine(" Hello! Please enter your IDcode");
+            IDCode = Console.ReadLine();
+            idValid = CheckIDLength(IDCode);
+            if (idValid)
+            {
+                Console.WriteLine("IDnr is OK");
+            }
+            else
+            {
+                Console.WriteLine("IDnr is NOT");
+            }
+            GetGender(IDCode);
+            CalculateAgeFromIDCode(IDCode);
+            /*
+            Console.WriteLine("Enter your sentence");
+            userSentence = Console.ReadLine();
+            //CountSpaces(userSentence);
+            */
+
+        }
+        /*public static void CountSpaces(string sentence)
+        {
+            int counter = 0;
+            foreach (char symbol in sentence)
+            {  
+                 if (symbol == ' ') {
+                    counter++;
+                }
+            }
+            Console.WriteLine($"The sentence contains {counter} of spaces");
+            Console.ReadLine();
+        }*/
+
+        public static void CalculateAgeFromIDCode(string IDnr)
+        {
+            string yearOfBirth;
+            yearOfBirth = IDnr.ToString().Substring(1, 2);
+            
+            DateTime birthDay = new DateTime();
+
+            Console.WriteLine($"Your year of birth is {yearOfBirth}");
+            Console.ReadLine();
+
+
+
+
+        }
+           public static void GetGender ( string idNumber)
+        {
+            string gender;
+            string firstNumber = idNumber[0].ToString();
+            if (firstNumber == "3" || firstNumber == "5")
+            {
+                gender = "male";
+                Console.WriteLine("Hello, Sir!");
+            }
+            else 
+            {
+                gender = "female";
+                Console.WriteLine("Hello, Madam!");
+            }
+            //Console.ReadLine();
+        }
+        public static bool  CheckIDLength (string ID)
+        {
+            bool isValid;
+            if (ID.Length == 11) {
+                isValid = true;
+            } else
+            {
+                isValid = false;
+            }
+            return isValid;
+        }
+    } 
+}
